226c55194921010778418b903c668a2d0124d68c
nolivermke <38256361+nolivermke@users.noreply.github.com>
2020-04-01 14:36:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,7 +93,10 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-
+  tags {
+    Billable=true
+    Deparment="devops"
+  }
   storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
