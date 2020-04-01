c1d1fe400569f5dec955753a51dff289c51b09ab
nolivermke <38256361+nolivermke@users.noreply.github.com>
2020-04-01 14:56:19 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -3,9 +3,9 @@
 module "network" {
   source  = "app.terraform.io/noliver-training/network/azurerm"
   version = "3.0.1"
-  name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.myresourcegroup.location
-  address_space       = [var.address_space]
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
 
